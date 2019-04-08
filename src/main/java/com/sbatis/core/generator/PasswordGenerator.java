c83caffbcb4c142e7868898f35a34f635020d2f0
huanglongpu <306315964@qq.com>
2019-04-08 17:52:56 +0800

    优化md5加密库

--- src/main/java/com/sbatis/core/generator/PasswordGenerator.java
+++ src/main/java/com/sbatis/core/generator/PasswordGenerator.java
@@ -6,52 +6,34 @@ import com.sbatis.encrypt.md5.Md5EncryptTool;
 
 import java.util.Date;
 
+/**
+ * md5密码生成库，建议sha值是个动态值，或者是不对用户展示的值，提高暴力破解的难度
+ * @author HuangLongPu
+ */
 public class PasswordGenerator {
 	
 	private PasswordGenerator() {}
 	
-	private static String PREFIX = "0000_";
-	private static String SUFFIX = "99+&123Q_WE";
+	private static String PREFIX = "00R9_";
+	private static String SUFFIX = "108+&C";
 
-	public static final String getPassword(String sha, String userName, String password) {
-		StringBuffer prePWD = new StringBuffer(Md5EncryptTool.encrypt16(PREFIX));
-		StringBuffer sufPWD = new StringBuffer(Md5EncryptTool.encrypt32(SUFFIX));
-
-		StringBuffer lastLoginTimePWD = new StringBuffer(Md5EncryptTool.encrypt16(sha));
-		StringBuffer userNamePWD = new StringBuffer(Md5EncryptTool.encrypt32(userName));
-		StringBuffer passwordPWD = new StringBuffer(Md5EncryptTool.encrypt16(password));
-
-		String prefixPWD = getPrefixPWD(prePWD, lastLoginTimePWD, userNamePWD);
-		String centerPWD = getCenterPWD(prePWD, sufPWD, lastLoginTimePWD, userNamePWD, passwordPWD);
-		String suffixPWD = getSuffixPWD(lastLoginTimePWD, passwordPWD, sufPWD);
-		return prefixPWD + centerPWD + suffixPWD
-				+ getBinaryBase(new StringBuilder(sha).append(userName)
-						.append(password).toString());
-	}
-
-	private static String getPrefixPWD(StringBuffer prePWD, StringBuffer lastLoginTimePWD, StringBuffer userNamePWD) {
-		return Md5EncryptTool.encrypt32(prePWD.append(lastLoginTimePWD).append(userNamePWD).toString().toUpperCase());
-	}
-
-	private static String getCenterPWD(StringBuffer prePWD, StringBuffer sufPWD, StringBuffer lastLoginTimePWD,
-			StringBuffer userNamePWD, StringBuffer passwordPWD) {
-		return Md5EncryptTool.encrypt16(Md5EncryptTool.encrypt32(
-				prePWD.append(lastLoginTimePWD).append(userNamePWD).append(passwordPWD).append(sufPWD).toString()));
-	}
-
-	private static String getSuffixPWD(StringBuffer lastLoginTimePWD, StringBuffer passwordPWD, StringBuffer sufPWD) {
-		return Md5EncryptTool.encrypt32(Md5EncryptTool.encrypt16(lastLoginTimePWD.append(passwordPWD).append(sufPWD).toString()));
+	public static final String getPassword(String shaName, String userName, String password) {
+		String prefixMd5Encrypt = Md5EncryptTool.encrypt16(PREFIX + shaName + userName);
+		String suffixMd5Encrypt = Md5EncryptTool.encrypt16( shaName + userName + SUFFIX);
+		String pwdMd5Encrypt = Md5EncryptTool.encrypt32(PREFIX + password + SUFFIX);
+		String minMd5Encrypt = Md5EncryptTool.encrypt32(prefixMd5Encrypt + pwdMd5Encrypt + suffixMd5Encrypt);
+		return getBinaryBase(prefixMd5Encrypt + pwdMd5Encrypt) + getBinaryBase(minMd5Encrypt);
 	}
 
 	private static String getBinaryBase(String str) {
 		char[] strChar = str.toCharArray();
-		String result = "";
+		String temp = "";
 
 		for (int i = 0; i < strChar.length; ++i) {
-			result = result + Integer.toBinaryString(strChar[i]) + " ";
+			temp = temp + Integer.toBinaryString(strChar[i]) + " ";
 		}
 
-		return new String(Base64EncoderTool.encode((Md5EncryptTool.encrypt16(result) + result.hashCode()).getBytes())).replace("M", "019a").replace("=", "").replace("j", "8").toLowerCase();
+		return new String(Base64EncoderTool.encode((Md5EncryptTool.encrypt32(temp) + Md5EncryptTool.encrypt16(temp.hashCode() + "")).getBytes())).replace("M", "a").replace("=", "").toLowerCase().replace("zz", "m").replace("tq", "a").replace("yy", "b").replace("nm", "a");
 	}
 
 	public static final boolean verifyPwd(String sha, String userName, String inputPassword, String password) {
