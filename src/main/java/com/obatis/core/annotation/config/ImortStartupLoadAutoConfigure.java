2cdb756a704d1069bb1c12e0ace9a78ff4caf286
huanglongpu <306315964@qq.com>
2019-05-24 14:06:35 +0800

    优化动态添加注解的逻辑

--- src/main/java/com/obatis/core/annotation/config/ImortStartupLoadAutoConfigure.java
+++ src/main/java/com/obatis/core/annotation/config/ImortStartupLoadAutoConfigure.java
@@ -1,6 +1,7 @@
 package com.obatis.core.annotation.config;
 
 import com.obatis.config.SystemConstant;
+import com.obatis.validate.ValidateTool;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.beans.factory.support.BeanDefinitionRegistry;
 import org.springframework.context.annotation.ClassPathScanningCandidateComponentProvider;
@@ -16,21 +17,22 @@ public class ImortStartupLoadAutoConfigure implements ImportBeanDefinitionRegist
 
         String startupClassName = annotationMetadata.getClassName();
         String startupPackageName = startupClassName.substring(0, startupClassName.lastIndexOf("."));
-        if("com".equals(startupPackageName) || SystemConstant.CORE_BASE_DIR.equals(startupPackageName)) {
+        if(ValidateTool.isEmpty(startupPackageName)) {
             return;
         }
-
-        /**
-         * 是否使用默认的filter，使用默认的filter意味着只扫描那些类上拥有Component、Service、Repository或Controller注解的类。
-         */
-        ClassPathScanningCandidateComponentProvider beanScanner = new ClassPathScanningCandidateComponentProvider(true);
-        Set<BeanDefinition> beanDefinitions = beanScanner.findCandidateComponents(startupPackageName);
-        for (BeanDefinition beanDefinition : beanDefinitions) {
+        if(!"com".equals(startupPackageName) && SystemConstant.CORE_BASE_DIR.equals(startupPackageName)) {
             /**
-             * beanName通常由对应的BeanNameGenerator来生成，比如Spring自带的AnnotationBeanNameGenerator、DefaultBeanNameGenerator等，也可以自己实现。
+             * 是否使用默认的filter，使用默认的filter意味着只扫描那些类上拥有Component、Service、Repository或Controller注解的类。
              */
-            String beanName = beanDefinition.getBeanClassName();
-            beanDefinitionRegistry.registerBeanDefinition(beanName, beanDefinition);
+            ClassPathScanningCandidateComponentProvider beanScanner = new ClassPathScanningCandidateComponentProvider(true);
+            Set<BeanDefinition> beanDefinitions = beanScanner.findCandidateComponents(startupPackageName);
+            for (BeanDefinition beanDefinition : beanDefinitions) {
+                /**
+                 * beanName通常由对应的BeanNameGenerator来生成，比如Spring自带的AnnotationBeanNameGenerator、DefaultBeanNameGenerator等，也可以自己实现。
+                 */
+                String beanName = beanDefinition.getBeanClassName();
+                beanDefinitionRegistry.registerBeanDefinition(beanName, beanDefinition);
+            }
         }
 
         /**
