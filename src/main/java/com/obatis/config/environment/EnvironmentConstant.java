b14520103cca240b841520eacf78e4e3c5ec3d03
huanglongpu <306315964@qq.com>
2020-02-27 12:47:24 +0800

    增加默认配置功能……

--- src/main/java/com/obatis/config/environment/EnvironmentConstant.java
+++ src/main/java/com/obatis/config/environment/EnvironmentConstant.java
@@ -5,8 +5,17 @@ public class EnvironmentConstant {
     protected static boolean ENVIRONMENT_LOAD_FLAG = false;
 
     protected enum SpringEn {
+        /**
+         * 是否配置访问404进入系统级拦截，不进行页面错误显示
+         */
         SPRING_NOT_FOUND("spring.mvc.throw-exception-if-no-handler-found", "true"),
+        /**
+         * 是否开启静态资源映射
+         */
         RESOURCES_ADD_MAPPING("spring.resources.add-mappings", "false"),
+        /**
+         * 数据库查询是否开启驼峰转化
+         */
         MYBATIS_MAP_UNDERSCORE("mybatis.configuration.map-underscore-to-camel-case", "true");
 
         private String key;
