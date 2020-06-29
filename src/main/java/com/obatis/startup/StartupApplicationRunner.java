d6ca9d7f3617568b1a1afde338e8fdb5715176af
huanglongpu <306315964@qq.com>
2020-06-29 10:59:18 +0800

    增加默认配置功能……

--- src/main/java/com/obatis/startup/StartupApplicationRunner.java
+++ src/main/java/com/obatis/startup/StartupApplicationRunner.java
@@ -12,14 +12,10 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.PropertiesPropertySource;
-import org.springframework.core.env.StandardEnvironment;
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
 
 import javax.annotation.Resource;
-import java.util.HashMap;
 import java.util.Map;
-import java.util.Properties;
 
 @Configuration
 public class StartupApplicationRunner implements ApplicationRunner {
