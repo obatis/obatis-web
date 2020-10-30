9630d92055973e4467e0e912aeeff34a76d9d62b
huanglongpu <306315964@qq.com>
2020-10-30 22:35:55 +0800

    增加默认配置功能……

--- src/main/java/com/obatis/config/time/LocalDateTimeConverterConfig.java
+++ src/main/java/com/obatis/config/time/LocalDateTimeConverterConfig.java
@@ -7,7 +7,7 @@ import org.springframework.core.convert.converter.Converter;
 import java.time.LocalDateTime;
 
 /**
- * 日时间转换处理类，体现为前端传入字符串时间类型时，也可以正常接收
+ * 时间转换处理类，体现为前端传入字符串时间类型时，也可以正常接收
  */
 @Configuration
 public class LocalDateTimeConverterConfig implements Converter<String, LocalDateTime> {
