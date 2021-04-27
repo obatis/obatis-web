da45da655c63b9ad0342baedcf42a7c3b2f21d85
huanglongpu <306315964@qq.com>
2021-04-27 23:55:38 +0800

    优化异常类

--- src/main/java/com/obatis/core/exception/NotAuthHandleException.java
+++ src/main/java/com/obatis/core/exception/NotAuthHandleException.java
@@ -8,25 +8,11 @@ public class NotAuthHandleException extends RuntimeException  {
 
 	public static final String DEFAUTL_NOT_AUTH_MESSAGE = "请求未授权，没有操作权限";
 
-    /**
-     * 异常信息
-     */
-    private String message;
-
     public NotAuthHandleException() {
-    	super(DEFAUTL_NOT_AUTH_MESSAGE);
+    	this(DEFAUTL_NOT_AUTH_MESSAGE);
 	}
     public NotAuthHandleException(String message) {
         super(message);
     }
-
-	public void setMessage(String message) {
-		this.message = message;
-	}
-	@Override
-	public String getMessage() {
-		return message;
-	}
-	
     
 }
--- src/main/java/com/obatis/core/exception/NotLoginHandleException.java
+++ src/main/java/com/obatis/core/exception/NotLoginHandleException.java
@@ -8,24 +8,11 @@ public class NotLoginHandleException extends RuntimeException {
 
 	public static final String DEFAULT_NOT_LOGIN_MESSAGE = "用户未登录";
 
-	/**
-	 * 异常信息
-	 */
-	private String message;
-
 	public NotLoginHandleException() {
-		super(DEFAULT_NOT_LOGIN_MESSAGE);
+		this(DEFAULT_NOT_LOGIN_MESSAGE);
 	}
 	public NotLoginHandleException(String message) {
 		super(message);
-	}
-
-	@Override
-	public String getMessage() {
-		return message;
-	}
-	public void setMessage(String message) {
-		this.message = message;
 	}
 
 }
